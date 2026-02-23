855fc977236a79a374d72b0ec1db1d99d7a89e8f
Fernando <4086244@alu.murciaeduca.es>
2026-02-23 20:47:37 +0100

    Documentacion de Personaje y Guerrero

--- Personaje.cs
+++ Personaje.cs
@@ -3,29 +3,47 @@ using System;
 namespace CIFPCarlosIII.ED.UT04.GrupoX
 
 {
+    /// <summary>
+    ///  Clase Personaje en el que se recoje cada estadistica y accion que cualquier personaje puede hacer.
+    /// </summary>
     public class Personaje
     {
+        /// <summary>
+        /// Atributos publicos. Estos sirven para despues nombrarlos en Metodos , propiedades, constructores....
+        /// </summary>
         public string nombre;
         public int vida;
         public int nivel;
-
+        
+        /// <summary>
+        /// Constructor, el cual le da unas estadisticas a cualquier personaje nuevo que se cree
+        /// </summary>
         public Personaje()
         {
             this.vida = 100;
             this.nivel = 1;
         }
-
+        /// <summary>
+        /// Metodo atacar, que hace que el personaje realice un ataque
+        /// </summary>
         public void Atacar()
         {
             Console.WriteLine(nombre + " realiza un ataque básico!");
         }
-
+        
+        /// <summary>
+        /// Metodo que realiza la accion de recibir daño.
+        /// </summary>
+        /// <param name="danio"></param>
         public void RecibirDanio(int danio)
         {
             vida -= danio;
             Console.WriteLine(nombre + " recibe " + danio + " de daño. Vida restante: " + vida);
         }
-
+        
+        /// <summary>
+        ///  Metodo que realiza la accion de subir de nivel. Cada vez que se suba, sube un nivel y sube 20 de vida
+        /// </summary>
         public void SubirNivel()
         {
             nivel++;
